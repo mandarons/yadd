f771b7a36e10d63ed0e11e5de1c63502bfd0f48b
Mandar Patil <mandarons@pm.me>
2021-08-19 07:56:44 -0700

    tests added

--- cypress/integration/service.spec.ts
+++ cypress/integration/service.spec.ts
@@ -6,6 +6,7 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
+
 describe('Service Flow', () => {
     const appURL = 'http://localhost:3334/';
     const newServiceData = {
@@ -15,6 +16,10 @@ describe('Service Flow', () => {
         logoURL: '/icons/adminer.png'
     };
 
+    before(() => {
+        cy.enableAuth(false);
+        cy.exec(`wait-on ${appURL}`);
+    });
     beforeEach(() => {
         cy.visit(appURL);
     });
@@ -82,5 +87,104 @@ describe('Service Flow', () => {
         cy.get(`#btnNewServiceClose`).click();
         deleteExistingService(newServiceData);
         verifyService(newServiceData, false);
+    });
+});
+describe('Service Flow - with Auth', () => {
+    const appURL = 'http://localhost:3334/';
+    const newServiceData = {
+        name: 'Google',
+        shortName: 'google',
+        url: 'https://google.com',
+        logoURL: '/icons/adminer.png'
+    };
+
+    before(() => {
+        cy.enableAuth(true);
+        cy.exec(`wait-on ${appURL}`);
+    });
+    beforeEach(() => {
+        cy.visit(appURL);
+    });
+    afterEach(async () => {
+
+    });
+    const createNewService = (service: { [key: string]: string; }) => {
+        cy.get('#btnAddNewService')
+            .click().get('#textNewService').should('have.text', 'New Service')
+            .get('#inputNewServiceName').type(service.name)
+            .get('#inputNewServiceURL').type(service.url)
+            .get('#inputNewServiceShortName').type(service.shortName)
+            .get('#inputNewServiceLogoURL').type(service.logoURL)
+            .get('#textNewServiceMessage').should('have.text', '')
+            .get('#btnNewServiceCreate').click();
+    };
+    const verifyService = (service: { [key: string]: string; }, exists = true) => {
+        if (exists) {
+            cy.get(`#status${newServiceData.shortName}`).should('not.be.null')
+                .get(`#link${newServiceData.shortName}`).should('have.attr', 'href').and('include', `/${service.shortName}`)
+                .get(`#img${newServiceData.shortName}`).should('have.attr', 'src').and('equal', service.logoURL)
+                .get(`#text${newServiceData.shortName}Name`).should('have.text', service.name)
+                .get(`#text${newServiceData.shortName}ShortName`).should('have.text', service.shortName);
+        } else {
+            cy.get(`#status${newServiceData.shortName}`).should('not.exist');
+        }
+
+    };
+    const deleteExistingService = (service: { [key: string]: string; }) => {
+        cy.get(`#btn${service.shortName}Edit`).click()
+            .get(`#btnEditService${service.shortName}Delete`).click()
+            .get(`#textEditService${service.shortName}Message`).should('not.be.empty')
+            .get(`#btnEditService${service.shortName}Delete`).click();
+    };
+    const login = () => cy.fixture("example").then(data => {
+        cy.get('#inputPassword').type(data.password)
+            .get('#btnLoginSubmit').click()
+            .get('#btnLogout').should('not.be.null')
+            .get('#btnAddNewService').should('not.be.null');
+    });
+    const logout = () => cy.get('#btnLogout').click();
+    it('should create a new service', () => {
+        login();
+        // Create new service
+        createNewService(newServiceData);
+        // Verify new service creation
+        verifyService(newServiceData);
+        logout();
+    });
+    it('should read existing service', () => {
+        login();
+        verifyService(newServiceData);
+        logout();
+    });
+    it('should update existing service', () => {
+        login();
+        const updatedName = newServiceData.name + 'updated';
+        // createNewService(newServiceData);
+        verifyService(newServiceData);
+        cy.get(`#btn${newServiceData.shortName}Edit`).click()
+            .get(`#inputEditService${newServiceData.shortName}Name`).type(updatedName)
+            .get(`#btnEditService${newServiceData.shortName}SaveChanges`).click();
+        verifyService({
+            ...newServiceData,
+            name: newServiceData.name + updatedName
+        });
+        logout();
+    });
+    it('should delete existing service', () => {
+        login();
+        deleteExistingService(newServiceData);
+        verifyService(newServiceData, false);
+        logout();
+    });
+    it('should not allow duplicate service creation', () => {
+        login();
+        createNewService(newServiceData);
+        verifyService(newServiceData);
+        createNewService(newServiceData);
+        cy.get(`#textNewServiceMessage`).should('not.be.empty');
+        cy.get(`#btnNewServiceClose`).click();
+        deleteExistingService(newServiceData);
+        verifyService(newServiceData, false);
+        logout();
     });
 });
