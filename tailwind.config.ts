77ecafb7d586f1f02e4843a089c5e9865d0b679c
Mandar Patil <mandarons@pm.me>
2024-01-11 15:25:59 +0000

    CRUD GUI working

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,32 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin'
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { yaddTheme } from './theme';
 
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
-		extend: {},
+		extend: {}
 	},
 	plugins: [
 		forms,
 		typography,
 		skeleton({
 			themes: {
+				custom: [yaddTheme],
 				preset: [
 					{
 						name: 'crimson',
-						enhancements: true,
-					},
-				],
-			},
-		}),
-	],
+						enhancements: true
+					}
+				]
+			}
+		})
+	]
 } satisfies Config;
